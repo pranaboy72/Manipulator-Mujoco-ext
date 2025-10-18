3ca9786ea50ac7b85e1a7ea00f834a717f860bf0
pranaboy72 <junwoochang@yonsei.ac.kr>
2025-10-18 19:09:21 +0900

    robotiq

--- manipulator_mujoco/robots/__init__.py
+++ manipulator_mujoco/robots/__init__.py
@@ -3,3 +3,4 @@ from manipulator_mujoco.robots.ag95 import AG95
 from manipulator_mujoco.robots.arm import Arm
 from manipulator_mujoco.robots.gripper import Gripper
 from manipulator_mujoco.robots.jaco_gen2 import JacoGen2
+from manipulator_mujoco.robots.robotiq_2f85 import Robotiq2F85
